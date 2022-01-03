b9cbe3b946f998458bd18e95d48f99326f55fd70
jaebin <jaebin.yang.99@gmail.com>
2022-01-03 14:45:58 +0900

    asdf

---
+++ Algo/src/bj/BJ_나무재태크_16235.java
@@ -0,0 +1,24 @@
+package bj;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class BJ_나무재태크_16235 {
+	static int N, M, K;
+	// r c는 1부터
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	}
+	
+	
+	static class Tree {
+		int y, x, age;
+		public Tree(int y, int x, int age) {
+			this.y = y;
+			this.x = x;
+			this.age = age;
+		}
+	}
+
+}
